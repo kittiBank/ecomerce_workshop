f2f56f0ce8f6381b60cbffa61e61b598b4f6f995
Kitti.Bank <kittitach.pro@gmail.com>
2025-04-03 22:30:44 +0700

    Add Status, Role, Cart function

--- controllers/user.js
+++ controllers/user.js
@@ -14,7 +14,7 @@ exports.listUsers = async (req, res) => {
             }
         })
         res.json(users)
-        res.send('Hello User Controller')
+        //  res.send('Hello User Controller')
     } catch (err) {
         console.log(err)
         res.status(500).json({ message: 'Server Error' })
@@ -24,50 +24,149 @@ exports.listUsers = async (req, res) => {
 exports.changeStatus = async (req, res) => {
     try {
 
-        res.send('Hello Change Status')
+        const { id, enabled } = req.body
+        console.log(id, enabled)
+        const user = await prisma.user.update({
+            where: { id: Number(id) },
+            data: { enabled: enabled }
+        })
+
+        res.send('Update Status Success')
     } catch (err) {
         console.log(err)
-        res.status(500).json({ message: 'Server Error'})
+        res.status(500).json({ message: 'Server Error' })
     }
 }
 
 exports.changeRole = async (req, res) => {
     try {
+        const { id, role } = req.body
 
-        res.send('Hello Change Role')
+        const user = await prisma.user.update({
+            where: { id: Number(id) },
+            data: { role: role }
+        })
+
+        res.send('Update Role Success')
     } catch (err) {
         console.log(err)
-        res.status(500).json({ message: 'Server Error'})
+        res.status(500).json({ message: 'Server Error' })
     }
 }
 
 exports.userCart = async (req, res) => {
     try {
 
-        res.send('Hello user Cart')
+        const { cart } = req.body
+        console.log(cart)
+        console.log(req.user.id)
+
+        const user = await prisma.user.findFirst({
+            where: { id: Number(req.user.id) }
+        })
+        //console.log(user)
+
+        //Delete old item in cart, before add new item in cart
+        await prisma.productOnCart.deleteMany({
+            where: {
+                cart: { orderById: user.id }
+            }
+        })
+
+        //Delete old cart, before add new cart
+        await prisma.cart.deleteMany({
+            where: { orderById: user.id }
+        })
+
+        // Prepare Product Mapping
+        let products = cart.map((item) => ({
+            productId: item.id,
+            count: item.count,
+            price: item.price
+        }))
+
+        //Cal sum price
+        let cartTotal = products.reduce((sum, item) => sum + item.price * item.count, 0)
+
+        //New cart
+        const newCart = await prisma.cart.create({
+            data: {
+                products: { create: products },
+                cartTotal: cartTotal,
+                orderById: user.id
+            }
+
+        })
+
+        console.log(newCart)
+
+        res.send('Add Cart Successful')
     } catch (err) {
         console.log(err)
-        res.status(500).json({ message: 'Server Error'})
+        res.status(500).json({ message: 'Server Error' })
     }
 }
 
 exports.getUserCart = async (req, res) => {
     try {
 
-        res.send('Hello getUserCart')
+        //req.user.id
+        const cart = await prisma.cart.findFirst({
+            where: {
+                orderById: Number(req.user.id)
+            },
+            include: {
+                products: {
+                    include: {
+                        product: true
+                    }
+                }
+            }
+        })
+
+        //console.log(cart)
+
+        res.json({
+            product: cart.products,
+            cartTotal: cart.cartTotal
+        })
     } catch (err) {
         console.log(err)
-        res.status(500).json({ message: 'Server Error'})
+        res.status(500).json({ message: 'Server Error' })
     }
 }
 
 exports.emptyCart = async (req, res) => {
     try {
+        //code
+        const cart = await prisma.cart.findFirst({
+            where: { orderById: Number(req.user.id) }
+        })
 
-        res.send('Hello emptyCart')
+        if (!cart) {
+            return res.status(400).json({ message: 'No cart' })
+        }
+
+        //เรียกใช้เลย ไม่ได้ประกาศตัวแปร เพราะไม่ได้เอาไปทำอะไรต่อ
+        await prisma.productOnCart.deleteMany({
+            where: {
+                cardId: cart.id
+            }
+        })
+
+        const result = await prisma.cart.deleteMany({
+            where: { orderById: Number(req.user.id) }
+        })
+
+
+        console.log(result)
+        res.json({
+            message: 'Cart Empty Success',
+            deletedCount: result.count
+        })
     } catch (err) {
         console.log(err)
-        res.status(500).json({ message: 'Server Error'})
+        res.status(500).json({ message: 'Server Error' })
     }
 }
 
@@ -77,7 +176,7 @@ exports.saveAddress = async (req, res) => {
         res.send('Hello saveAddress')
     } catch (err) {
         console.log(err)
-        res.status(500).json({ message: 'Server Error'})
+        res.status(500).json({ message: 'Server Error' })
     }
 }
 
@@ -87,7 +186,7 @@ exports.saveOrder = async (req, res) => {
         res.send('Hello saveOrder')
     } catch (err) {
         console.log(err)
-        res.status(500).json({ message: 'Server Error'})
+        res.status(500).json({ message: 'Server Error' })
     }
 }
 
@@ -97,6 +196,6 @@ exports.getOrder = async (req, res) => {
         res.send('Hello getOrder')
     } catch (err) {
         console.log(err)
-        res.status(500).json({ message: 'Server Error'})
+        res.status(500).json({ message: 'Server Error' })
     }
 }
