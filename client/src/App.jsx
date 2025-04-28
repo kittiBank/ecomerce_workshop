bf9f76a47e1e4c8706d0ad0d073861a42e8ed5db
Kitti.Bank <kittitach.pro@gmail.com>
2025-04-28 21:35:49 +0700

    Create Client page router

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+//rafce
+import React from 'react'
+import AppRoutes from './routes/AppRoutes'
+
+const App = () => {
+  //Java script
+
+  return (
+    //JSX Retrun HTML+JS
+    <>
+      <AppRoutes/>
+    </>
+  )
+}
+
+export default App
